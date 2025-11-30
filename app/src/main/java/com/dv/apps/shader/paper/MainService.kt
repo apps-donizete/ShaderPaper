b26f0597e37425fe637bb3adbbb1ba63b399df50
Donizete Vida <donizetevida1998@outlook.com>
2025-11-30 22:57:10 +0100

    Fix suggestions given by my friend chatti gepiti

--- app/src/main/java/com/dv/apps/shader/paper/MainService.kt
+++ app/src/main/java/com/dv/apps/shader/paper/MainService.kt
@@ -5,6 +5,7 @@ import android.opengl.GLES20.*
 import android.opengl.GLSurfaceView
 import android.os.SystemClock
 import android.service.wallpaper.WallpaperService
+import android.util.Log
 import android.view.SurfaceHolder
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
@@ -109,17 +110,19 @@ class MainService : WallpaperService() {
             private var iResolutionId = -1
             private var iTimeId = -1
 
-            private val fullQuadVertexBuffer = ByteBuffer
+            private val fullScreenTriangleBuffer = ByteBuffer
                 .allocateDirect(6 * 4)
                 .order(ByteOrder.nativeOrder())
-                .also {
-                    it.asFloatBuffer().put(
+                .asFloatBuffer()
+                .apply {
+                    put(
                         floatArrayOf(
                             -3f, -1f,
                             1f, -1f,
                             1f, 3f
                         )
                     )
+                    position(0)
                 }
 
             override fun onSurfaceCreated(
@@ -134,7 +137,7 @@ class MainService : WallpaperService() {
                         gl_Position = vPosition;
                     }
                     """.trimIndent()
-                } ?: -1
+                }
 
                 fragmentId = loadShader(GL_FRAGMENT_SHADER) {
                     """
@@ -162,49 +165,42 @@ class MainService : WallpaperService() {
                         }
                     }
                     """.trimIndent()
-                } ?: -1
-
-                programId = glCreateProgram()
-
-                glAttachShader(programId, vertexId)
-                glAttachShader(programId, fragmentId)
-
-                glBindAttribLocation(programId, 0, "vPosition")
-
-                glLinkProgram(programId)
-
-                val buffer = intArrayOf(-1)
-                glGetProgramiv(programId, GL_LINK_STATUS, buffer, 0)
-
-                if (buffer[0] == 0) {
-                    val reason = glGetProgramInfoLog(programId)
-                    android.util.Log.d("GLSLWallpaper", "Failed to link program: $reason")
-                    glDeleteProgram(programId)
-                    return
                 }
 
-                glUseProgram(programId)
+                programId = glCreateProgram().also {
+                    glAttachShader(it, vertexId)
+                    glAttachShader(it, fragmentId)
+                    glBindAttribLocation(it, 0, "vPosition")
+                    glLinkProgram(it)
 
-                glEnableVertexAttribArray(0)
-                glVertexAttribPointer(
-                    0,
-                    2,
-                    GL_FLOAT,
-                    false,
-                    0,
-                    fullQuadVertexBuffer
-                )
+                    val linkStatus = intArrayOf(-1)
+                    glGetProgramiv(it, GL_LINK_STATUS, linkStatus, 0)
+
+                    if (linkStatus[0] == 0) {
+                        glDeleteProgram(programId)
+
+                        val reason = glGetProgramInfoLog(programId)
+                        Log.e("GLSLWallpaper", "Failed to link program: $reason")
+
+                        throw IllegalStateException("Failed to link program")
+                    }
+                }
+
+                glDeleteShader(vertexId)
+                glDeleteShader(fragmentId)
+
+                glUseProgram(programId)
 
                 iResolutionId = glGetUniformLocation(programId, "iResolution")
                 iTimeId = glGetUniformLocation(programId, "iTime")
+
+                glEnableVertexAttribArray(0)
+                glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, fullScreenTriangleBuffer)
             }
 
             override fun onDrawFrame(gl: GL10) {
-                val time = SystemClock.uptimeMillis() / 1000f
-
                 glUseProgram(programId)
-                glUniform1f(iTimeId, time)
-
+                glUniform1f(iTimeId, SystemClock.uptimeMillis() / 1000f)
                 glDrawArrays(GL_TRIANGLES, 0, 3)
             }
 
@@ -214,24 +210,31 @@ class MainService : WallpaperService() {
                 height: Int
             ) {
                 glViewport(0, 0, width, height)
+                glUseProgram(programId)
                 glUniform2f(iResolutionId, width.toFloat(), height.toFloat())
             }
 
             private fun loadShader(
                 type: Int,
                 source: () -> String,
-            ): Int? {
+            ): Int {
                 val id = glCreateShader(type)
+
                 glShaderSource(id, source())
                 glCompileShader(id)
-                val buffer = intArrayOf(-1)
-                glGetShaderiv(id, GL_COMPILE_STATUS, buffer, 0)
-                if (buffer[0] == 0) {
-                    val reason = glGetShaderInfoLog(id)
-                    android.util.Log.d("GLSLWallpaper", "Failed to compile shader: $reason")
+
+                val compileStatus = intArrayOf(-1)
+                glGetShaderiv(id, GL_COMPILE_STATUS, compileStatus, 0)
+
+                if (compileStatus[0] == 0) {
                     glDeleteShader(id)
-                    return null
+
+                    val reason = glGetShaderInfoLog(id)
+                    Log.e("GLSLWallpaper", "Failed to compile shader: $reason")
+
+                    throw IllegalStateException("Failed to compile shader")
                 }
+
                 return id
             }
         }
