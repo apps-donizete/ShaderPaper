e1f4458c259cc2c0972fd7d2a2463b938609c6ec
Donizete Vida <donizetevida1998@outlook.com>
2025-11-30 18:58:52 +0100

    Draw a RED wallpaper

--- app/src/main/java/com/dv/apps/shader/paper/MainActivity.kt
+++ app/src/main/java/com/dv/apps/shader/paper/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.dv.apps.shader.paper
 
+import android.app.WallpaperManager
+import android.content.ComponentName
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.dv.apps.shader.paper.ui.theme.ShaderPaperTheme
 
@@ -20,28 +26,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShaderPaperTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        Modifier.padding(innerPadding)
+                    ) {
+                        val context = LocalContext.current
+                        Button(
+                            onClick = {
+                                val intent = Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER)
+                                    .apply {
+                                        putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+                                            ComponentName(
+                                                context,
+                                                MainService::class.java
+                                            )
+                                        )
+                                    }
+                                context.startActivity(intent)
+                            }
+                        ) {
+                            Text("Click me")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ShaderPaperTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/dv/apps/shader/paper/MainService.kt
+++ app/src/main/java/com/dv/apps/shader/paper/MainService.kt
@@ -1,11 +1,46 @@
 package com.dv.apps.shader.paper
 
+import android.graphics.Color
 import android.service.wallpaper.WallpaperService
+import android.view.SurfaceHolder
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
 
 class MainService : WallpaperService() {
     override fun onCreateEngine() = EngineImpl()
 
-    inner class EngineImpl : Engine() {
+    inner class EngineImpl : Engine(), LifecycleOwner {
+        override val lifecycle: LifecycleRegistry = LifecycleRegistry(this)
 
+        override fun onCreate(surfaceHolder: SurfaceHolder) {
+            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+        }
+
+        override fun onSurfaceCreated(holder: SurfaceHolder) {
+            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_START)
+
+            val canvas = holder.lockCanvas()
+            canvas.drawColor(Color.RED)
+            holder.unlockCanvasAndPost(canvas)
+        }
+
+        override fun onSurfaceChanged(
+            holder: SurfaceHolder,
+            format: Int,
+            width: Int,
+            height: Int
+        ) {
+            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        }
+
+        override fun onSurfaceDestroyed(holder: SurfaceHolder) {
+            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
+        }
+
+        override fun onDestroy() {
+            lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+        }
     }
 }
