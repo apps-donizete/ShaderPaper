6de5bd7a7c95cf549c0b70ad4d43c3a8731ae550
Donizete Vida <donizetevida1998@outlook.com>
2025-11-30 20:33:54 +0100

    Code cleanup

--- app/src/main/java/com/dv/apps/shader/paper/MainService.kt
+++ app/src/main/java/com/dv/apps/shader/paper/MainService.kt
@@ -1,11 +1,15 @@
 package com.dv.apps.shader.paper
 
+import android.content.Context
 import android.opengl.GLSurfaceView
 import android.service.wallpaper.WallpaperService
 import android.view.SurfaceHolder
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -14,42 +18,26 @@ class MainService : WallpaperService() {
 
     inner class EngineImpl : Engine(), LifecycleOwner {
         override val lifecycle: LifecycleRegistry = LifecycleRegistry(this)
+        private val glslWallpaper = GLSLWallpaper(applicationContext, this)
 
         override fun onCreate(surfaceHolder: SurfaceHolder) {
             lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+
+            lifecycleScope.launch {
+                repeatOnLifecycle(Lifecycle.State.CREATED) {
+                    glslWallpaper.onCreate()
+                }
+                repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                    glslWallpaper.onResume()
+                }
+                repeatOnLifecycle(Lifecycle.State.DESTROYED) {
+                    glslWallpaper.onDestroy()
+                }
+            }
         }
 
         override fun onSurfaceCreated(holder: SurfaceHolder) {
             lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_START)
-
-            val glSurfaceView = object : GLSurfaceView(applicationContext) {
-                override fun getHolder() = holder
-            }
-
-            glSurfaceView.setEGLContextClientVersion(2)
-            glSurfaceView.preserveEGLContextOnPause = true
-
-            glSurfaceView.setRenderer(object: GLSurfaceView.Renderer {
-                override fun onDrawFrame(gl: GL10) {
-                    gl.glClearColor(1f, 0f, 0f, 1f)
-                    gl.glClear(GL10.GL_COLOR_BUFFER_BIT)
-                }
-
-                override fun onSurfaceChanged(
-                    gl: GL10,
-                    width: Int,
-                    height: Int
-                ) {
-                }
-
-                override fun onSurfaceCreated(
-                    gl: GL10,
-                    config: EGLConfig
-                ) {
-                }
-            })
-
-            glSurfaceView.onResume()
         }
 
         override fun onSurfaceChanged(
@@ -68,6 +56,65 @@ class MainService : WallpaperService() {
 
         override fun onDestroy() {
             lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+        }
+    }
+
+    private class GLSLWallpaper(
+        private val context: Context,
+        private val engine: Engine
+    ) {
+        private var glSurfaceView: GLSurfaceViewImpl? = null
+
+        suspend fun onCreate() {
+            glSurfaceView = object : GLSurfaceViewImpl(context) {
+                override fun getHolder() = engine.surfaceHolder
+            }
+            glSurfaceView?.setEGLContextClientVersion(2)
+            glSurfaceView?.preserveEGLContextOnPause = true
+            glSurfaceView?.setRenderer(redPainter)
+        }
+
+        suspend fun onResume() {
+            glSurfaceView?.onResume()
+        }
+
+        suspend fun onPause() {
+            glSurfaceView?.onPause()
+        }
+
+        suspend fun onDestroy() {
+            glSurfaceView?.onDestroy()
+            glSurfaceView = null
+        }
+
+        private abstract class GLSurfaceViewImpl(
+            context: Context
+        ) : GLSurfaceView(context) {
+            abstract override fun getHolder(): SurfaceHolder
+
+            fun onDestroy() {
+                onDetachedFromWindow()
+            }
+        }
+
+        private val redPainter = object : GLSurfaceView.Renderer {
+            override fun onDrawFrame(gl: GL10) {
+                gl.glClearColor(1f, 0f, 0f, 1f)
+                gl.glClear(GL10.GL_COLOR_BUFFER_BIT)
+            }
+
+            override fun onSurfaceChanged(
+                gl: GL10,
+                width: Int,
+                height: Int
+            ) {
+            }
+
+            override fun onSurfaceCreated(
+                gl: GL10,
+                config: EGLConfig
+            ) {
+            }
         }
     }
 }
