09656b644c778c28aae799dc9dfbfb4b30b39b58
Donizete Vida <donizetevida1998@outlook.com>
2025-12-03 00:53:10 +0100

    Add retrofit example

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
+    implementation(libs.bundles.retrofit)
+
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
 
--- app/src/main/java/com/dv/apps/shader/paper/MainActivity.kt
+++ app/src/main/java/com/dv/apps/shader/paper/MainActivity.kt
@@ -13,11 +13,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.dv.apps.shader.paper.ui.theme.ShaderPaperTheme
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.create
+import retrofit2.http.GET
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +40,8 @@ class MainActivity : ComponentActivity() {
                             onClick = {
                                 val intent = Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER)
                                     .apply {
-                                        putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+                                        putExtra(
+                                            WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
                                             ComponentName(
                                                 context,
                                                 MainService::class.java
@@ -50,5 +57,25 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        lifecycleScope.launch {
+            val retrofit: Retrofit = Retrofit.Builder()
+                .client(
+                    OkHttpClient.Builder()
+                        .addInterceptor(HttpLoggingInterceptor())
+                        .build()
+                )
+                .baseUrl("https://raw.githubusercontent.com/apps-donizete/ShaderPaper/refs/heads/main/")
+                .build()
+            val repository = retrofit.create<Repository>()
+            val data = repository.getData()
+            val content = data.string()
+            println(content)
+        }
     }
+}
+
+interface Repository {
+    @GET("repository/data.txt")
+    suspend fun getData(): ResponseBody
 }
