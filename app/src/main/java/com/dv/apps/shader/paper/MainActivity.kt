6f964550b5542900dd1e79aafb35d5305ea1f704
Donizete Vida <donizetevida1998@outlook.com>
2025-12-03 03:03:21 +0100

    Add WallpaperService button back

--- app/src/main/java/com/dv/apps/shader/paper/MainActivity.kt
+++ app/src/main/java/com/dv/apps/shader/paper/MainActivity.kt
@@ -54,6 +54,9 @@ class MainActivity : ComponentActivity() {
                                 it
                             )
                         }
+                        item {
+                            ApplyItem()
+                        }
                     }
                 }
             }
