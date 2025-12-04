25fe5b03649b49340513a4f87938d04b6104a72a
Donizete Vida <donizetevida1998@outlook.com>
2025-12-04 02:31:13 +0100

    Use navigation3 DSL call

--- app/src/main/java/com/dv/apps/shader/paper/feature/navigation/MainNavigation.kt
+++ app/src/main/java/com/dv/apps/shader/paper/feature/navigation/MainNavigation.kt
@@ -3,27 +3,25 @@ package com.dv.apps.shader.paper.feature.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
 import com.dv.apps.shader.paper.feature.home.HomeScreen
 
 @Composable
 fun MainNavigation() {
-    val backStack = rememberSaveable { mutableStateListOf(Destination.HOME) }
+    val backStack = rememberSaveable { mutableStateListOf<Entry>(Entry.Home) }
 
     NavDisplay(
         backStack = backStack,
         onBack = { backStack.removeLastOrNull() },
-        entryProvider = { key ->
-            when (key) {
-                Destination.HOME -> NavEntry(key) {
-                    HomeScreen()
-                }
+        entryProvider = entryProvider {
+            entry<Entry.Home> {
+                HomeScreen()
             }
         }
     )
 }
 
-enum class Destination {
-    HOME
+sealed interface Entry {
+    data object Home : Entry
 }
